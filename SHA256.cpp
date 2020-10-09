3c41ae7a62d86082ee410243f2f06292e1cb0172
Morgan <melon21@hotmail.se>
2020-10-09 14:09:18 +0200

    stuff that isn't working but is still there

--- SHA256.cpp
+++ SHA256.cpp
@@ -2,20 +2,46 @@
 #include <bitset>
 #include <cstdint>
 #include <iostream>
+#include "SHA256.h"
 
-inline void SHR32(long long& val)
+class Digest
 {
-	val >> 32;
+	uint64_t val;
+public:
+	Digest(uint64_t val);
+	void shr(int32_t shift);
+	void rotr(int32_t shift);
+
+	void sigma0();
+	void sigma1();
+
+	void usigma0();
+	void usigma1();
+};
+
+Digest::Digest(uint64_t val) : val(val){}
+
+inline void Digest::shr(int32_t shift)
+{
+	val >> shift;
 }
 
-std::uint64_t rotr(std::uint64_t v, std::int32_t shift) {
-	std::int32_t s = shift >= 0 ? shift % 32 : -((-shift) % 32);
-	return (v >> s) | (v << (32 - s));
+inline void Digest::rotr(int32_t shift) {
+	int32_t s = shift >= 0 ? shift % 32 : -((-shift) % 32);
+	val = (val >> s) | (val << (32 - s));
 }
+
+inline void Digest::sigma0()
+{
+
+}
+
 int main()
 {
 	using namespace std;
-	cout << bitset<64>(rotr(1, 2)) << endl; // 4
+	uint64_t val = 3;
+	Digest::rotr(val, 2);
+	cout << bitset<4>(val) << endl; // 4
 	
 	cout << bitset<4>(2) << endl;
 	cout << bitset<4>(1) << endl;
